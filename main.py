f86eb3c6162621e7701e4d49306bf5016fb7fba2
nurislamkalmanbetov <nurislam_96@mail.ru>
2024-05-06 08:42:52 +0600

    add more crud requests

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 
 app = FastAPI()
 
@@ -54,3 +55,36 @@ async def get_food(food_name: FoodEnum):
             }
     
     return {'food_name': food_name, 'message': 'i like chocolate milk'}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"},]
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut consectetur"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        if isinstance(item, dict):  # Проверка, что item является словарем
+            item.update(
+                {"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, Ut consectetur"}
+            )
+    return item
+
+                        
